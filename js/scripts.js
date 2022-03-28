0eac037aad82357eedab2415de79fe7dddced121
William Rodriguez <william@rodriguez1.com>
2022-03-28 09:46:29 -0700

    Fix spacing
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>
    Co-authored-by: Timothy Gibson <ryaninlux@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,12 @@ $(document).ready(function() {
       newArray2.push(newArray[1]);
       newArray2.push(newArray[0]);
       newArray2.push(newArray[2]);
-      let list = $("<ul></ul>");
       
-
+      let list = $("<ul></ul>");
       list.append($("<li>" + newArray2[0] + "</li>"));
       list.append($("<li>" + newArray2[1] + "</li>"));
       list.append($("<li>" + newArray2[2] + "</li>"));
-        
+      
       $("#fav-list").text("");
       $("#fav-list").append(list);
     });
