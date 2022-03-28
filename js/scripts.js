80d8a3159302433da7ce8a74282b43f9d35c8434
William Rodriguez <william@rodriguez1.com>
2022-03-28 09:45:16 -0700

    Fix indentation
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>
    Co-authored-by: Timothy Gibson <ryaninlux@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
     $("form").submit(function(event) {
-    event.preventDefault();
-    const valueOne = $("#thing1").val();
-    const valueTwo = $("#thing2").val();
-    const valueThree = $("#thing3").val();
+      event.preventDefault();
+      const valueOne = $("#thing1").val();
+      const valueTwo = $("#thing2").val();
+      const valueThree = $("#thing3").val();
 
-    const newArray = [valueOne, valueTwo, valueThree];
-    const newArray2 = [];
-    newArray2.push(newArray[1]);
-    newArray2.push(newArray[0]);
-    newArray2.push(newArray[2]);
-    let list = $("<ul></ul>");
-    
-
-    list.append($("<li>" + newArray2[0] + "</li>"));
-    list.append($("<li>" + newArray2[1] + "</li>"));
-    list.append($("<li>" + newArray2[2] + "</li>"));
+      const newArray = [valueOne, valueTwo, valueThree];
+      const newArray2 = [];
+      newArray2.push(newArray[1]);
+      newArray2.push(newArray[0]);
+      newArray2.push(newArray[2]);
+      let list = $("<ul></ul>");
       
-    $("#fav-list").text("");
-    $("#fav-list").append(list);
 
-  });
+      list.append($("<li>" + newArray2[0] + "</li>"));
+      list.append($("<li>" + newArray2[1] + "</li>"));
+      list.append($("<li>" + newArray2[2] + "</li>"));
+        
+      $("#fav-list").text("");
+      $("#fav-list").append(list);
+    });
   });
